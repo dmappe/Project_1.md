fdfad82efa1cd7ab9ea02c62401d7b7ce9f5d3a8
dmappe <diegomappe@gmail.com>
2025-12-07 11:15:16 -0500

    paso 4

---
+++ README.md/app.py
@@ -0,0 +1,36 @@
+import pandas as pd
+import plotly.express as px
+import streamlit as st
+
+st.header('Proyecto Sprint 7 - Diego Mappe')
+st.write('Esta aplicación aún no es funcional. En construcción')
+
+car_data = pd.read_csv('vehicles_us.csv')
+hist_button = st.button('Construir histograma')
+
+if hist_button:
+    st.write(
+        'Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
+    fig = px.histogram(car_data, x='odometer')
+    st.plotly_chart(fig, use_container_width=True)
+
+scatter_button = st.button('Construir gráfico de dispersión')
+
+if scatter_button:
+    st.write('Creación de un gráfico de dispersión para el conjunto de datos de anuncios de venta de coches')
+    fig = px.scatter(car_data, x='odometer', y='price')
+    st.plotly_chart(fig, use_container_width=True)
+
+build_histogram = st.checkbox('Construir un histograma')
+
+if build_histogram:
+    st.write('Construir un histograma para la columna odómetro')
+    fig = px.histogram(car_data, x='odometer')
+    st.plotly_chart(fig, use_container_width=True)
+
+build_scatter = st.checkbox('Construir un gráfico de dispersión')
+
+if build_scatter:
+    st.write('Construir un gráfico de dispersión para la columna odómetro')
+    fig = px.scatter(car_data, x='odometer', y='price')
+    st.plotly_chart(fig, use_container_width=True)
